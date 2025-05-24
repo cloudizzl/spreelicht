0472632a01f466820e37f715959485ab5e08d005
Max-REP0 <madmexxa.mj@gmail.com>
2025-05-24 17:34:52 +0200

    Added constants for the screens and set it up for now. Moved header and footer outside the body tag for better readability

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,13 @@
+const LOGIN_SCREEN = document.getElementById("login-screen")
+const MAIN_SCREEN = document.getElementById("main-screen")
+const ADD_SCREEN = document.getElementById("add-screen")
+const UPDATE_SCREEN = document.getElementById("update-screen")
+
+LOGIN_SCREEN.style.display ="block";
+MAIN_SCREEN.style.display ="none";
+ADD_SCREEN.style.display ="none";
+UPDATE_SCREEN.style.display ="none";
+
 function login(e) {
     e.preventDefault();
     const username = document.getElementById("username").value;
